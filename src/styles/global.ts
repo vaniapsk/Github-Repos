f41a0dd20be3703c93f948872aa6f0606d42fe09
Vania Oliveira <vaniawtc@users.noreply.github.com>
2020-09-19 22:36:59 -0700

    Added background immage

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,5 +1,7 @@
 import { createGlobalStyle } from 'styled-components';
 
+import githubBackground from '../assets/github-background.svg';
+
 export default createGlobalStyle`
   * {
     margin: 0;
@@ -8,12 +10,23 @@ export default createGlobalStyle`
     box-sizing : border-box;
   }
   body{
-    background: #F0F0F5;
+    background: #F0F0F5 url(${githubBackground}) no-repeat 70% top;
     -webkit-font-smoothing: antialiased;
+
 
   }
   body, input, button{
-    font: 16px sans-serif;
+    font: 16px Roboto, sans-serif;
+  }
+
+  #root{
+    max-width: 968px;
+    margin: 0 auto;
+    padding: 40px 20px;
+  }
+
+  button{
+    cursor: pointer;
   }
 
 `;
