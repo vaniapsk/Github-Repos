3aef05e0bd1f699a1b9bcb1aea66c6dbcf62a522
Vania Oliveira <vaniawtc@users.noreply.github.com>
2020-09-19 22:13:22 -0700

    New font style

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import { Title } from './styles';
+
+// Component Dashboard
+const Dashboard: React.FC = () => {
+  return <Title> Explore repositories on github</Title>;
+};
+
+export default Dashboard;
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+
+// Component Dashboard
+const Rerpository: React.FC = () => {
+  return <h1> Rerpository</h1>;
+};
+
+export default Rerpository;
