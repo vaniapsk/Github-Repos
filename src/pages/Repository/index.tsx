a1d23b1578b77fe649481a4190d7de14b84262e3
Vania Oliveira <vaniawtc@users.noreply.github.com>
2020-09-27 16:40:23 -0700

    Finished Project

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,8 +1,119 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link, useRouteMatch } from 'react-router-dom';
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi';
+import api from '../../services/api';
+
+import logoImg from '../../assets/logo.svg';
+
+import { Header, RepositoryInfo, Issues } from './styles';
+
+interface RepositoryParams {
+  repository: string;
+}
+
+interface Repository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  };
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  };
+}
 
 // Component Dashboard
 const Rerpository: React.FC = () => {
-  return <h1> Rerpository</h1>;
+  const [repository, setRepository] = useState<Repository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+
+  // to send params to another route
+  const { params } = useRouteMatch<RepositoryParams>();
+
+  useEffect(() => {
+    // Good way of getting request (both requests happens at the same time!)
+    api.get(`repos/${params.repository}`).then(response => {
+      setRepository(response.data);
+    });
+    api.get(`repos/${params.repository}/issues`).then(response => {
+      setIssues(response.data);
+    });
+    // Also Good way of getting request (both requests happens at the same time!)
+    // async function loadData(): Promise<void> {
+    //   const [repository, issues] = await Promise.all([
+    //     api.get(`repos/${params.repository}`),
+    //     api.get(`repos/${params.repository}/issues`),
+    //   ]);
+    //   console.log(repository);
+    //   console.log(issues);
+    // }
+    // loadData();
+  }, [params.repository]);
+
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Gihub explorer" />
+        <Link to="/Dashboard">
+          <FiChevronLeft size={16} />
+          Back
+        </Link>
+      </Header>
+
+      {repository ? (
+        <RepositoryInfo>
+          <header>
+            <img
+              src={repository.owner.avatar_url}
+              alt={repository.owner.login}
+            />
+            <div>
+              <strong>{repository.full_name}</strong>
+              <p>{repository.description}</p>
+            </div>
+          </header>
+          <ul>
+            <li>
+              <strong>{repository.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{repository.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{repository.open_issues_count}</strong>
+              <span>Open Issues</span>
+            </li>
+          </ul>
+        </RepositoryInfo>
+      ) : (
+        <p>Loading...</p>
+      )}
+      <Issues>
+        {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+      </Issues>
+    </>
+  );
 };
 
 export default Rerpository;
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,8 @@ import Repository from '../pages/Repository';
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/" component={Repository} />
+    <Route path="/Dashboard" exact component={Dashboard} />
+    <Route path="/repositories/:repository+" component={Repository} />
   </Switch>
 );
 
